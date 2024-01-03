894bf0dfe41f445f04f69157a931152fb7004ac8
mbadox45 <89310302+mbadox45@users.noreply.github.com>
2024-01-03 09:58:32 +0700

    Update env.js

--- src/api/env.js
+++ src/api/env.js
@@ -1,8 +1,8 @@
-export const URL_WEB = 'http://localhost:8087/';
-export const URL_API = 'http://localhost:3002/api/';
-export const URL_WEB_Portal = 'https://portalapps.inl.co.id/';
-export const URL_API_Portal = 'http://localhost:3000/api/';
-export const URL_API_Odoo = 'http://localhost:3003/api/';
+export const URL_WEB = 'your_url';
+export const URL_API = 'your_url';
+export const URL_WEB_Portal = 'your_url';
+export const URL_API_Portal = 'your_url';
+export const URL_API_Odoo = 'your_url';
 
 
 export const APP_ID = '15';
